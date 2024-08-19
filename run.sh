a121a4997d20556dc451950e7e91d34716f05aba
Nerorrlex911 <nerorrlex911@outlook.com>
2024-08-19 13:43:27 +0800

    open tensorboard automatically

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 pip install tensorboard tensorboardX
 python main.py --ncpu 50 --device 0,1,2,3,4,5 --nconf 80 --lr 0.00001 --data_path train --epochs 600
 tensorboard --logdir=logs/train
+firefox http://localhost:6006
