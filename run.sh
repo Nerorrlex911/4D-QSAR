c12f2b49122fae730f68d10f5674cbb6ea5c643d
Nerorrlex911 <nerorrlex911@outlook.com>
2024-08-19 21:19:31 +0800

    requirements.txt

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
-pip install tensorboard tensorboardX
-python main.py --ncpu 50 --device 0,1,2,3,4,5 --nconf 80 --lr 0.00001 --data_path train --epochs 600
-tensorboard --logdir=logs/train
-firefox http://localhost:6006
+# pip install tensorboard tensorboardX
+# python main.py --ncpu 50 --device 0,1,2,3,4,5 --nconf 80 --lr 0.00001 --data_path train --epochs 600
+# tensorboard --logdir=logs/train
+# firefox http://localhost:6006
+pip install -r requirements.txt
+pip install torch==1.13.1+cu117 torchvision==0.14.1+cu117 torchaudio==0.13.1 --extra-index-url https://download.pytorch.org/whl/cu117
+conda install openbabel -c conda-forge
